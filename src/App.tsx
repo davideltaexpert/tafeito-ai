672e0ce6676c456ada47c79d9364652e9bf9ed19
Davi Hilario de Oliveira <davi@deltaexpertfranca.com.br>
2023-10-21 14:16:12 -0300

    Atualização feita até a Aula do dia 21/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import React, { ReactNode }  from 'react';
-import './App.css';
-import AuthProvider from './provider/authProvider';
-import Routes from './routes';
+import React, { ReactNode, useState } from "react";
+import "./App.css";
+import AuthProvider from "./provider/authProvider";
+import { SnackbarProvider } from "notistack";
+import { MyGlobalContext } from "./utils/global";
+import Routes from "./routes";
+import { useStateWithRef } from "./utils/hooks";
+
 
 function App() {
+  const [isEditingTask, setIsEditingTask] = useState<boolean>(false);
+  const [selectedTaskInput, setSelectedTaskInput] = useState<string | null>(
+    null
+  );
+  const [refetchtaskStatus, setRefectchTaskStatus] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [softDeletedTasks, setSoftDeletedTasks, softDeletedTasksRef] = useStateWithRef([]);
+
+
   return (
     <div className="App">
       <AuthProvider>
-        <Routes />
+      <SnackbarProvider maxSnack={3}>
+          <MyGlobalContext.Provider
+            value={{
+              isEditingTask,
+              setIsEditingTask,
+              selectedTaskInput,
+              setSelectedTaskInput,
+              refetchtaskStatus,
+              setRefectchTaskStatus,
+              isLoading,
+              setIsLoading,
+              softDeletedTasks,
+              setSoftDeletedTasks,
+              softDeletedTasksRef,
+            }}
+          >
+            <Routes />
+          </MyGlobalContext.Provider>
+        </SnackbarProvider>
       </AuthProvider>
-
     </div>
   );
 }
